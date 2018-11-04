e3507029f818783400d8ffa9e40862ebfecb9591
Alessandro Scoppio <alessandroscoppio@gmail.com>
2018-11-04 18:24:49 +0100

    starting writing voting schemes functions

--- src/Main.py
+++ src/Main.py
@@ -8,7 +8,17 @@ candidates = range(nCandidates)
 
 preferences = {}
 
+# ouput (giorgos says that maybe this should be in a function, probably he's right
 for voter in range(nVoters):
     preferences[voter] = sample(candidates, len(candidates))
     print("For voter " +str(voter)+ " the preferences are: " +str(preferences[voter]))
 
+
+
+### implementation of voting schemes
+
+# PLURALITY VOTING
+def plurality_voting() :
+
+
+preferences = function()
