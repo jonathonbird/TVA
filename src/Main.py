67b4b801df1396bc07452bdc31d13437d6f24696
Rodrigo Brechard <rodrigobrechard@gmail.com>
2018-11-08 10:32:43 +0100

    Voting schemes

--- src/Main.py
+++ src/Main.py
@@ -10,6 +10,12 @@ candidates = [chr(i) for i in range(ord('A'), ord('A') + nCandidates)]
 
 preferences = {}
 
+# Dictionary with the output of the vote
+voting = {}
+for candidate in range(ord('A'), ord('A') + nCandidates):
+    voting[chr(candidate)] = 0
+
+
 # ouput (giorgos says that maybe this should be in a function, probably he's right
 for voter in range(nVoters):
     preferences[voter] = sample(candidates, len(candidates))
@@ -18,11 +24,33 @@ for voter in range(nVoters):
 
 
 ### implementation of voting schemes
+def voting_for_two():
+    for voter, preference in preferences.items():
+        voting[preference[0]] += 1;
+        voting[preference[1]] += 1;
+
+def plurality_voting():
+    for voter, preference in preferences.items():
+        voting[preference[0]] += 1;
+
+def anti_plurality_voting():
+    # print(preferences)
+    for voter, preference in preferences.items():
+        for i, p in enumerate(preference):
+            if i is len(preference) - 1:
+                break
+            voting[p] += 1;
+
+def borda_voting():
+    for voter, preference in preferences.items():
+        for i, p in enumerate(preference):
+            voting[p] += len(preference) - 1 - i;
 
 # PLURALITY VOTING
-def plurality_voting(preferencesMatrix) :
+# def plurality_voting(preferencesMatrix) :
 
-    for voter in range(nVoters):
-        # if preferences[0, voter] work in progress don't mess up plz
-
-preferences = function()
+    # for voter in range(nVoters):
+         # if preferences[0, voter] work in progress don't mess up plz
+borda_voting()
+# preferences = function()
+print(voting)
