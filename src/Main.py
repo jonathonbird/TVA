840ffb948f7636f3fb758684d12cdd57676d39d5
Rodrigo Brechard <rodrigobrechard@gmail.com>
2018-11-12 10:56:25 +0100

    plurality voting for Push Over

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,10 @@
 # import numpy as np
 from random import sample
 from Voting_Scheme import VotingScheme
+from Push_Over import  PushOver
 
-nVoters = 5
-nCandidates = 5
+nVoters = 10
+nCandidates = 4
 
 # this is not good, we need candidates to be in the form: {A, B, C, ...}
 # candidates = range(nCandidates)
@@ -16,13 +17,14 @@ voting = {}
 # ouput (giorgos says that maybe this should be in a function, probably he's right
 for voter in range(nVoters):
     preferences[voter] = sample(candidates, len(candidates))
-    print("For voter " + str(voter) + " the preferences are: " + str(preferences[voter]))
+    print("For voter", voter, "the preferences are:", preferences[voter])
 
-vote = VotingScheme(preferences, nCandidates)
+# vote = VotingScheme(preferences, nCandidates)
 # print(vote.plurality_voting())
 # print(vote.voting_for_two())
 # print(vote.anti_plurality_voting())
-vote.borda_voting()
+# vote.borda_voting()
 # print('OUTCOME: \n', vote.get_outcome())
-print("The overall happiness is", vote.calc_overall_happiness())
+# print("The overall happiness is", vote.calc_overall_happiness())
 
+PushOver(preferences, 0)
